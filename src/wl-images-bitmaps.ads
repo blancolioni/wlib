075ff45a9ccf3637255b98227edfc28ecf6fd5a8
Fraser Wilson <blancolioni@gmail.com>
2012-11-09 14:54:56 +0100

    added WL.Images package hierarchy, starting with DDS and bitmap

---
+++ src/wl-images-bitmaps.ads
@@ -0,0 +1,8 @@
+package WL.Images.Bitmaps is
+
+   procedure Write_Bitmap (Image : Image_Type'Class;
+                           Path  : String;
+                           Level : Image_Level_Index :=
+                             Default_Image_Level);
+
+end WL.Images.Bitmaps;
---
+++ src/wl-images-dds.ads
@@ -0,0 +1,6 @@
+package WL.Images.DDS is
+
+   procedure Read_DDS (Image : out Image_Type'Class;
+                       Path  : in  String);
+
+end WL.Images.DDS;
