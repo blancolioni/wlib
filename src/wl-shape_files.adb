9ccff5b556af53f16ca3446076e959c47dfc1d64
Fraser Wilson <blancolioni@gmail.com>
2014-03-01 11:34:54 +0100

    WL.Shape_Files: various fixes

--- src/wl-shape_files.adb
+++ src/wl-shape_files.adb
@@ -4,6 +4,8 @@ with System.Storage_Elements;
 
 package body WL.Shape_Files is
 
+   Debug_Shapes : constant Boolean := False;
+
    type Record_Header is
       record
          Number     : WL.Binary_IO.Word_32;
@@ -53,7 +55,8 @@ package body WL.Shape_Files is
       return Boolean
    is
    begin
-      return WL.Binary_IO.End_Of_File (File.File);
+      return File.Shapes.Is_Empty
+        and then WL.Binary_IO.End_Of_File (File.File);
    end End_Of_File;
 
    ----------
@@ -64,75 +67,96 @@ package body WL.Shape_Files is
      (File : in out File_Type)
       return Shape_Type
    is
-      use System.Storage_Elements;
-      Header : Record_Header;
-      Buffer : Storage_Array (0 .. 11);
-      for Buffer'Address use Header'Address;
    begin
-      WL.Binary_IO.Read (File.File, Buffer);
-      Change_Endian (Buffer (0 .. 3));
-      Change_Endian (Buffer (4 .. 7));
-      if True then
-         Ada.Text_IO.Put_Line
-           ("record" & Header.Number'Img
-            & " length" & Header.Length'Img
-            & " shape "
-            & Shape_Class'Image (To_Shape_Class (Header.Shape_Type)));
+      if File.Shapes.Is_Empty then
+         declare
+            use System.Storage_Elements;
+            Header : Record_Header;
+            Buffer : Storage_Array (0 .. 11);
+            for Buffer'Address use Header'Address;
+         begin
+            WL.Binary_IO.Read (File.File, Buffer);
+            Change_Endian (Buffer (0 .. 3));
+            Change_Endian (Buffer (4 .. 7));
+            if Debug_Shapes then
+               Ada.Text_IO.Put_Line
+                 ("record" & Header.Number'Img
+                  & " length" & Header.Length'Img
+                  & " shape "
+                  & Shape_Class'Image (To_Shape_Class (Header.Shape_Type)));
+            end if;
+
+            declare
+               Shape : Shape_Type (To_Shape_Class (Header.Shape_Type));
+            begin
+               case Shape.Class is
+               when Null_Shape =>
+                  null;
+               when Point_Shape =>
+                  WL.Binary_IO.Read (File.File, 64, Shape.X'Address);
+                  WL.Binary_IO.Read (File.File, 64, Shape.Y'Address);
+               when Polygon_Shape | Polyline_Shape =>
+                  WL.Binary_IO.Read (File.File, 256,
+                                     Shape.Polygon_Bound'Address);
+                  declare
+                     use WL.Binary_IO;
+                     Num_Parts, Num_Points : WL.Binary_IO.Word_32;
+                  begin
+                     WL.Binary_IO.Read (File.File, Num_Parts);
+                     WL.Binary_IO.Read (File.File, Num_Points);
+                     if Debug_Shapes then
+                        Ada.Text_IO.Put_Line
+                          ("num parts =" & Num_Parts'Img
+                           & "; num points =" & Num_Points'Img);
+                     end if;
+
+                     declare
+                        Skips : array (1 .. Num_Parts) of Word_32;
+                        Pts   : array (1 .. Num_Points) of Shape_Point;
+                     begin
+                        for I in 1 .. Num_Parts loop
+                           WL.Binary_IO.Read (File.File, Skips (I));
+                        end loop;
+                        for I in 1 .. Num_Points loop
+                           declare
+                              Pt  : Shape_Point;
+                           begin
+                              WL.Binary_IO.Read (File.File, 128, Pt'Address);
+                              Pts (I) := Pt;
+                           end;
+                        end loop;
+
+                        for I in 1 .. Num_Parts loop
+                           declare
+                              S : Shape_Type (Shape.Class);
+                              Lo : constant Word_32 :=
+                                     Skips (I) + 1;
+                              Hi : constant Word_32 :=
+                                     (if I < Num_Parts
+                                      then Skips (I + 1)
+                                      else Num_Points);
+                           begin
+                              for J in Lo .. Hi loop
+                                 S.Polygon_Points.Append (Pts (J));
+                              end loop;
+                              File.Shapes.Append (S);
+                           end;
+                        end loop;
+                     end;
+                  end;
+
+               end case;
+            end;
+         end;
       end if;
+
       declare
-         Result : Shape_Type (To_Shape_Class (Header.Shape_Type));
+         Result : constant Shape_Type := File.Shapes.First_Element;
       begin
-         case Result.Class is
-            when Null_Shape =>
-               null;
-            when Point_Shape =>
-               WL.Binary_IO.Read (File.File, 64, Result.X'Address);
-               WL.Binary_IO.Read (File.File, 64, Result.Y'Address);
-            when Polygon_Shape | Polyline_Shape =>
-               WL.Binary_IO.Read (File.File, 256,
-                                  Result.Polygon_Bound'Address);
---                 Ada.Text_IO.Put_Line
---                   ("min x "
---                    & Integer'Image (Integer (Result.Polygon_Bound.Min_X)));
---                 Ada.Text_IO.Put_Line
---                   ("min y "
---                    & Integer'Image (Integer (Result.Polygon_Bound.Min_Y)));
---                 Ada.Text_IO.Put_Line
---                   ("max x "
---                    & Integer'Image (Integer (Result.Polygon_Bound.Max_X)));
---                 Ada.Text_IO.Put_Line
---                   ("max y "
---                    & Integer'Image (Integer (Result.Polygon_Bound.Max_Y)));
-               declare
-                  Num_Parts, Num_Points : WL.Binary_IO.Word_32;
-               begin
-                  WL.Binary_IO.Read (File.File, Num_Parts);
-                  WL.Binary_IO.Read (File.File, Num_Points);
-                  Ada.Text_IO.Put_Line
-                    ("num parts =" & Num_Parts'Img
-                     & "; num points =" & Num_Points'Img);
-                  for I in 1 .. Num_Parts loop
-                     declare
-                        Skip : WL.Binary_IO.Word_32;
-                     begin
-                        WL.Binary_IO.Read (File.File, Skip);
-                        if False then
-                           Ada.Text_IO.Put_Line ("skip:" & Skip'Img);
-                        end if;
-                     end;
-                  end loop;
-                  for I in 1 .. Natural (Num_Points) loop
-                     declare
-                        Pt : Shape_Point;
-                     begin
-                        WL.Binary_IO.Read (File.File, 128, Pt'Address);
-                        Result.Polygon_Points.Append (Pt);
-                     end;
-                  end loop;
-               end;
-         end case;
+         File.Shapes.Delete_First;
          return Result;
       end;
+
    end Next;
 
    ----------
@@ -143,23 +167,28 @@ package body WL.Shape_Files is
      (File : in out File_Type;
       Name : in     String)
    is
-      Buffer : System.Storage_Elements.Storage_Array (0 .. 99);
-      for Buffer'Address use File.Header'Address;
-
    begin
-      WL.Binary_IO.Open
-        (File => File.File,
-         Mode => WL.Binary_IO.In_File,
-         Name => Name);
-      WL.Binary_IO.Read
-        (File.File, Buffer);
-      Change_Endian (Buffer (0 .. 3));
-      Change_Endian (Buffer (24 .. 27));
+      File.Header := new File_Header;
 
-      Ada.Text_IO.Put_Line ("File length:" & File.Header.Length'Img);
-      Ada.Text_IO.Put_Line ("File contents: "
-                            & Shape_Class'Image
-                              (To_Shape_Class (File.Header.Shape_Type)));
+      declare
+         Buffer : System.Storage_Elements.Storage_Array (0 .. 99);
+         for Buffer'Address use File.Header.all'Address;
+
+      begin
+         WL.Binary_IO.Open
+           (File => File.File,
+            Mode => WL.Binary_IO.In_File,
+            Name => Name);
+         WL.Binary_IO.Read
+           (File.File, Buffer);
+         Change_Endian (Buffer (0 .. 3));
+         Change_Endian (Buffer (24 .. 27));
+
+         Ada.Text_IO.Put_Line ("File length:" & File.Header.Length'Img);
+         Ada.Text_IO.Put_Line ("File contents: "
+                               & Shape_Class'Image
+                                 (To_Shape_Class (File.Header.Shape_Type)));
+      end;
 
    end Open;
 
