091d0cc627d4ddb8520eee82a2fee612efa5aa77
Fraser Wilson <blancolioni@gmail.com>
2014-03-01 11:34:44 +0100

    WL.Random: save/restore state via strings

--- src/wl-random.adb
+++ src/wl-random.adb
@@ -30,6 +30,21 @@ package body WL.Random is
 
    G : My_Random.Generator;
 
+   -------------------
+   -- Current_State --
+   -------------------
+
+   function Current_State return String is
+      State : My_Random.State;
+   begin
+      My_Random.Save (G, State);
+      return My_Random.Image (State);
+   end Current_State;
+
+   -------------------
+   -- Random_Number --
+   -------------------
+
    function Random_Number (Max : Natural) return Natural is
    begin
       if Max = 0 then
@@ -38,6 +53,10 @@ package body WL.Random is
          return My_Random.Random (G) mod Max;
       end if;
    end Random_Number;
+
+   -------------------
+   -- Random_Number --
+   -------------------
 
    function Random_Number (Min, Max : Integer) return Integer is
    begin
@@ -48,6 +67,10 @@ package body WL.Random is
       end if;
    end Random_Number;
 
+   ---------------
+   -- Randomise --
+   ---------------
+
    procedure Randomise is
       use Calendar;
       Seed : Integer;
@@ -57,6 +80,16 @@ package body WL.Random is
       Seed := Integer (Seconds (Now)) * 10;
       My_Random.Reset (G, Seed);
    end Randomise;
+
+   -------------------
+   -- Restore_State --
+   -------------------
+
+   procedure Restore_State (State : String) is
+      Restored_State : constant My_Random.State := My_Random.Value (State);
+   begin
+      My_Random.Reset (G, Restored_State);
+   end Restore_State;
 
 begin
    My_Random.Reset (G, 0);
