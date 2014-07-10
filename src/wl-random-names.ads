a72ae5f2d45a12240dc50eeb8635bf6c1fba5057
Fraser Wilson <fwilson@anago.nl>
2014-07-10 16:08:27 +0200

    Added package WL.Random.Names, which implements random name generation using vowel and consonant lists.  Sample configuration supplied.

---
+++ src/wl-random-names.ads
@@ -0,0 +1,36 @@
+private with Ada.Containers.Vectors;
+
+package WL.Random.Names is
+
+   type Name_Generator is private;
+
+   procedure Load_Lexicon
+     (Generator       :    out Name_Generator;
+      Vowels_Path     : in     String;
+      Consonants_Path : in String);
+
+
+   function Random_Name
+     (Generator : Name_Generator)
+      return String;
+
+private
+
+   type Lexeme_Info is
+      record
+         Lexeme : String (1 .. 3) := (others => ' ');
+         Can_Middle : Boolean := True;
+         Can_Begin : Boolean := True;
+         Can_End : Boolean := True;
+      end record;
+
+   package Lexeme_Info_Vectors is
+     new Ada.Containers.Vectors (Positive, Lexeme_Info);
+
+   type Name_Generator is
+      record
+         Vowels     : Lexeme_Info_Vectors.Vector;
+         Consonants : Lexeme_Info_Vectors.Vector;
+      end record;
+
+end WL.Random.Names;
