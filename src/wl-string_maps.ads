445553aa600a47314681ccbb7a2a6541bf7ee7bb
Fraser Wilson <blancolioni@gmail.com>
2016-09-15 17:24:35 +0200

    New package WL.String_Maps (Ada.Containers.Indefinite_Hashed_Maps instantiated for case-insensitive strings)

---
+++ src/wl-string_maps.ads
@@ -0,0 +1,31 @@
+with Ada.Containers.Indefinite_Hashed_Maps;
+with Ada.Strings.Fixed.Equal_Case_Insensitive;
+with Ada.Strings.Fixed.Hash_Case_Insensitive;
+
+generic
+   type Element_Type (<>) is private;
+   with function "=" (Left, Right : Element_Type) return Boolean is <>;
+package WL.String_Maps is
+
+   package Maps is
+     new Ada.Containers.Indefinite_Hashed_Maps
+       (Key_Type        => String,
+        Element_Type    => Element_Type,
+        Hash            => Ada.Strings.Fixed.Hash_Case_Insensitive,
+        Equivalent_Keys => Ada.Strings.Fixed.Equal_Case_Insensitive,
+        "="             => "=");
+
+   type Map is new Maps.Map with null record;
+
+   subtype Cursor is Maps.Cursor;
+
+   function Has_Element (Position : Cursor) return Boolean
+                         renames Maps.Has_Element;
+
+   function Key (Position : Cursor) return String
+                 renames Maps.Key;
+
+   function Element (Position : Cursor) return Element_Type
+                     renames Maps.Element;
+
+end WL.String_Maps;
