b305eba3ddbd3a720985e52b33f81017fae73d18
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2017-05-09 12:00:52 +0200

    Added WL versions of Money and Quantities

---
+++ src/wl-money.adb
@@ -0,0 +1,372 @@
+with Ada.Strings.Fixed;
+
+package body WL.Money is
+
+   Local_Currency_Symbol       : String := "$   ";
+   Local_Digit_Grouping_Symbol : String := ",   ";
+   Local_Decimal_Symbol        : String := ".   ";
+
+   -------------
+   -- Add_Tax --
+   -------------
+
+   function Add_Tax (Money    : Money_Type;
+                     Tax_Rate : Float)
+                     return Money_Type
+   is
+   begin
+      if Money > 0 then
+         return Money + Tax (Money, Tax_Rate);
+      else
+         return Money;
+      end if;
+   end Add_Tax;
+
+   -------------
+   -- Add_Tax --
+   -------------
+
+   function Add_Tax
+     (Price : Price_Type;
+      Tax_Rate : Float)
+      return Price_Type
+   is
+   begin
+      return Price_Type (Add_Tax (Money_Type (Price), Tax_Rate));
+   end Add_Tax;
+
+   ------------
+   -- Adjust --
+   ------------
+
+   function Adjust (Money    : Money_Type;
+                    Factor   : Float)
+                    return Money_Type
+   is
+   begin
+      return Money * Money_Type (Factor);
+   end Adjust;
+
+   ------------------
+   -- Adjust_Price --
+   ------------------
+
+   function Adjust_Price (Price    : Price_Type;
+                          Factor   : Float)
+                         return Price_Type
+   is
+   begin
+      return Price_Type (Float (Price) * Factor);
+   end Adjust_Price;
+
+   ---------------------
+   -- Currency_Symbol --
+   ---------------------
+
+   function Currency_Symbol return String is
+   begin
+      return Ada.Strings.Fixed.Trim
+        (Local_Currency_Symbol, Ada.Strings.Right);
+   end Currency_Symbol;
+
+   --------------------
+   -- Decimal_Symbol --
+   --------------------
+
+   function Decimal_Symbol return String is
+   begin
+      return Ada.Strings.Fixed.Trim
+        (Local_Decimal_Symbol, Ada.Strings.Right);
+   end Decimal_Symbol;
+
+   ---------------------------
+   -- Digit_Grouping_Symbol --
+   ---------------------------
+
+   function Digit_Grouping_Symbol return String is
+   begin
+      return Ada.Strings.Fixed.Trim
+        (Local_Digit_Grouping_Symbol, Ada.Strings.Right);
+   end Digit_Grouping_Symbol;
+
+   ------------------
+   -- Get_Quantity --
+   ------------------
+
+   function Get_Quantity
+     (Total_Cash : Money_Type;
+      Price      : Price_Type)
+      return Quantities.Quantity_Type
+   is
+   begin
+      return Quantities.To_Quantity
+        (Float (Total_Cash) / Float (Price));
+   end Get_Quantity;
+
+   -----------
+   -- Image --
+   -----------
+
+   function Image (Item : Money_Type) return String is
+   begin
+      if Item < 0 then
+         return "(" & Image (Price_Type (abs Item)) & ")";
+      else
+         return Image (Price_Type (Item));
+      end if;
+   end Image;
+
+   -----------
+   -- Image --
+   -----------
+
+   function Image (Item : Price_Type) return String is
+      Image : constant String :=
+        Ada.Strings.Fixed.Trim (Price_Type'Image ((Item + 5) / 10),
+                                Ada.Strings.Left);
+      Currency : constant String := Currency_Symbol;
+   begin
+      if Image'Length = 1 then
+         return Currency & "0.0" & Image;
+      elsif Image'Length = 2 then
+         return Currency & "0." & Image;
+      else
+         return Currency & Image (Image'First .. Image'Last - 2) & "." &
+           Image (Image'Last - 1 .. Image'Last);
+      end if;
+   end Image;
+
+   ---------
+   -- Max --
+   ---------
+
+   function Max (X, Y : Money_Type) return Money_Type is
+   begin
+      return Money_Type'Max (X, Y);
+   end Max;
+
+   ---------
+   -- Max --
+   ---------
+
+   function Max (X, Y : Price_Type) return Price_Type is
+   begin
+      return Price_Type'Max (X, Y);
+   end Max;
+
+   ---------
+   -- Min --
+   ---------
+
+   function Min (X, Y : Money_Type) return Money_Type is
+   begin
+      return Money_Type'Min (X, Y);
+   end Min;
+
+   ---------
+   -- Min --
+   ---------
+
+   function Min (X, Y : Price_Type) return Price_Type is
+   begin
+      return Price_Type'Min (X, Y);
+   end Min;
+
+   -----------
+   -- Price --
+   -----------
+
+   function Price (Total    : Money_Type;
+                   Quantity_Type : Quantities.Quantity_Type)
+                   return Price_Type
+   is
+      use type Quantities.Quantity_Type;
+   begin
+      if Quantity_Type = Quantities.Zero then
+         return Zero;
+      else
+         return Price_Type
+           (Float (Total) / Quantities.To_Float (Quantity_Type));
+      end if;
+   end Price;
+
+   --------------------------
+   -- Set_Image_Properties --
+   --------------------------
+
+   procedure Set_Image_Properties
+     (Currency_Symbol       : String := "$";
+      Digit_Grouping_Symbol : String := ",";
+      Decimal_Symbol        : String := ".")
+   is
+   begin
+      Local_Currency_Symbol (1 .. Currency_Symbol'Length) :=
+        Currency_Symbol;
+      Local_Digit_Grouping_Symbol (1 .. Digit_Grouping_Symbol'Length) :=
+        Digit_Grouping_Symbol;
+      Local_Decimal_Symbol (1 .. Decimal_Symbol'Length) :=
+        Decimal_Symbol;
+   end Set_Image_Properties;
+
+   -----------
+   -- Split --
+   -----------
+
+   function Split
+     (Amount  : Money_Type;
+      Portion : Float)
+      return Money_Type
+   is
+   begin
+      return Money_Type (Float (Amount) * Portion);
+   end Split;
+
+   ---------
+   -- Tax --
+   ---------
+
+   function Tax (Money : Money_Type;
+                 Tax   : Float)
+                 return Money_Type
+   is
+   begin
+      if Money < 0 then
+         return 0;
+      else
+         return Money_Type (Float (Money) * Tax);
+      end if;
+   end Tax;
+
+   ---------
+   -- Tax --
+   ---------
+
+   function Tax (Price   : Price_Type;
+                            Tax     : Float)
+                            return Price_Type
+   is
+   begin
+      return Price_Type (Float (Price) * Tax);
+   end Tax;
+
+   --------------
+   -- To_Float --
+   --------------
+
+   function To_Float (Amount : Money_Type) return Float is
+   begin
+      return Float (Amount) / 1000.0;
+   end To_Float;
+
+   --------------
+   -- To_Money --
+   --------------
+
+   function To_Money (Amount : Float) return Money_Type is
+   begin
+      return Money_Type (Amount * 1000.0);
+   end To_Money;
+
+   -----------
+   -- Total --
+   -----------
+
+   function Total (Price  : Price_Type;
+                   Quantity_Type : Quantities.Quantity_Type)
+                  return Money_Type
+   is
+      use Quantities;
+   begin
+      return Money_Type (Float (Price) * To_Float (Quantity_Type));
+   end Total;
+
+   -----------
+   -- Value --
+   -----------
+
+   function Value (Image : String) return Money_Type is
+      Currency : constant String := Currency_Symbol;
+   begin
+      if Image = "" then
+         return Zero;
+      elsif Image (Image'First) = '~' then
+         declare
+            Result : constant Money_Type :=
+                       Value (Image (Image'First + 1 .. Image'Last));
+         begin
+            return Result;
+         end;
+      elsif Image'Length > Currency'Length
+        and then Image (Image'First .. Image'First + Currency'Length - 1)
+        = Currency
+      then
+         return Value (Image (Image'First + Currency'Length .. Image'Last));
+      elsif Image (Image'Last) = 'K' then
+         return Value (Image (Image'First .. Image'Last - 1)) * 1E3;
+      elsif Image (Image'Last) = 'M' then
+         return Value (Image (Image'First .. Image'Last - 1)) * 1E6;
+      elsif Image (Image'Last) = 'G' then
+         return Value (Image (Image'First .. Image'Last - 1)) * 1E9;
+      else
+         return Money_Type (Float'Value (Image));
+      end if;
+   end Value;
+
+   -----------
+   -- Value --
+   -----------
+
+   function Value (Image : String) return Price_Type is
+   begin
+      return To_Price (Float'Value (Image));
+   end Value;
+
+   -----------------
+   -- Without_Tax --
+   -----------------
+
+   function Without_Tax
+     (Money : Money_Type;
+      Tax   : Float)
+      return Money_Type
+   is
+   begin
+      if Money < 0 then
+         return Money;
+      else
+         return Money_Type (Float (Money) / (1.0 + Tax));
+      end if;
+   end Without_Tax;
+
+   -----------------
+   -- Without_Tax --
+   -----------------
+
+   function Without_Tax
+     (Price   : Price_Type;
+      Tax     : Float)
+      return Price_Type
+   is
+   begin
+      return Price_Type (Without_Tax (Money_Type (Price), Tax));
+   end Without_Tax;
+
+   ----------
+   -- Zero --
+   ----------
+
+   function Zero return Money_Type is
+   begin
+      return 0;
+   end Zero;
+
+   ----------
+   -- Zero --
+   ----------
+
+   function Zero return Price_Type is
+   begin
+      return 0;
+   end Zero;
+
+end WL.Money;
---
+++ src/wl-money.ads
@@ -0,0 +1,126 @@
+with WL.Quantities;
+
+package WL.Money is
+
+   type Money_Type is private;
+
+   function Zero return Money_Type;
+
+   function "+" (Left, Right : Money_Type) return Money_Type;
+   function "-" (Left, Right : Money_Type) return Money_Type;
+
+   function "<"  (Left, Right : Money_Type) return Boolean;
+   function ">"  (Left, Right : Money_Type) return Boolean;
+   function "<=" (Left, Right : Money_Type) return Boolean;
+   function ">=" (Left, Right : Money_Type) return Boolean;
+
+   function "abs" (Left : Money_Type) return Money_Type;
+
+   function Adjust (Money    : Money_Type;
+                    Factor   : Float)
+                   return Money_Type;
+
+   function To_Money (Amount : Float) return Money_Type;
+   function To_Float (Amount : Money_Type) return Float;
+
+   function Max (X, Y : Money_Type) return Money_Type;
+   function Min (X, Y : Money_Type) return Money_Type;
+
+   function Tax (Money : Money_Type;
+                 Tax   : Float)
+                 return Money_Type;
+
+   function Without_Tax (Money : Money_Type;
+                         Tax   : Float)
+                         return Money_Type;
+
+   function Add_Tax (Money    : Money_Type;
+                     Tax_Rate : Float)
+                     return Money_Type;
+
+   type Price_Type is private;
+
+   function "+" (Left, Right : Price_Type) return Price_Type;
+   function "-" (Left, Right : Price_Type) return Price_Type;
+
+   function "<"  (Left, Right : Price_Type) return Boolean;
+   function ">"  (Left, Right : Price_Type) return Boolean;
+   function "<=" (Left, Right : Price_Type) return Boolean;
+   function ">=" (Left, Right : Price_Type) return Boolean;
+
+   function To_Price (Amount : Float) return Price_Type;
+   function To_Float (Price : Price_Type) return Float;
+
+   function Adjust_Price (Price    : Price_Type;
+                          Factor   : Float)
+                         return Price_Type;
+
+   function Tax (Price   : Price_Type;
+                 Tax     : Float)
+                 return Price_Type;
+
+   function Without_Tax (Price   : Price_Type;
+                         Tax     : Float)
+                         return Price_Type;
+
+   function Add_Tax (Price : Price_Type;
+      Tax_Rate : Float)
+                     return Price_Type;
+
+   function Max (X, Y : Price_Type) return Price_Type;
+   function Min (X, Y : Price_Type) return Price_Type;
+
+   function Total (Price    : Price_Type;
+                   Quantity_Type : Quantities.Quantity_Type)
+                  return Money_Type;
+
+   function Price (Total    : Money_Type;
+                   Quantity_Type : Quantities.Quantity_Type)
+                   return Price_Type;
+
+   function Get_Quantity
+     (Total_Cash : Money_Type;
+      Price      : Price_Type)
+      return Quantities.Quantity_Type;
+
+   function Image (Item : Money_Type) return String;
+   function Image (Item : Price_Type) return String;
+
+   function Value (Image : String) return Money_Type;
+   function Value (Image : String) return Price_Type;
+
+   function Split (Amount  : Money_Type;
+                   Portion : Float)
+                  return Money_Type;
+
+   function Zero return Price_Type;
+
+   function Currency_Symbol return String;
+   function Digit_Grouping_Symbol return String;
+   function Decimal_Symbol return String;
+
+   procedure Set_Image_Properties
+     (Currency_Symbol       : String := "$";
+      Digit_Grouping_Symbol : String := ",";
+      Decimal_Symbol        : String := ".");
+
+private
+
+   type Money_Type is range -2 ** 63 .. 2 ** 63 - 1;
+   type Price_Type is range 0 .. 2 ** 63 - 1;
+
+   function To_Price (Amount : Float) return Price_Type
+   is (Price_Type (To_Money (Amount)));
+
+   function To_Float (Price : Price_Type) return Float
+   is (To_Float (Money_Type (Price)));
+
+   pragma Import (Intrinsic, "+");
+   pragma Import (Intrinsic, "-");
+   pragma Import (Intrinsic, "<");
+   pragma Import (Intrinsic, ">");
+   pragma Import (Intrinsic, "<=");
+   pragma Import (Intrinsic, ">=");
+   pragma Import (Intrinsic, "abs");
+
+end WL.Money;
