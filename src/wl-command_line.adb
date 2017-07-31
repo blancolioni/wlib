f338d3da5043251fa807d7fb025f73535062aaf0
Fraser Wilson <blancolioni@gmail.com>
2017-07-31 12:47:08 +0200

    WL.Command_Line: simplified package; now a low-level search interface to Ada.Command_Line

--- src/wl-command_line.adb
+++ src/wl-command_line.adb
@@ -1,317 +1,97 @@
 with Ada.Command_Line;
-with Ada.Containers.Indefinite_Vectors;
-with Ada.Containers.Vectors;
-with Ada.Text_IO;
+with Ada.Strings.Fixed;
 
 package body WL.Command_Line is
 
-   type Registered_Handler is
-      record
-         Handler    : access Root_Argument_Handler'Class;
-         Long_Name  : access String;
-         Short_Name : Character;
-      end record;
+   -----------------
+   -- Find_Option --
+   -----------------
 
-   package Handler_Vectors is
-      new Ada.Containers.Vectors (Positive, Registered_Handler);
-
-   package String_Vectors is
-      new Ada.Containers.Indefinite_Vectors (Positive, String);
-
-   Handlers    : Handler_Vectors.Vector;
-   Dispatchers : Handler_Vectors.Vector;
-
-   Processed_Command_Line : String_Vectors.Vector;
-
-   type Default_Handler is
-     new Root_Argument_Handler with
-      record
-         Bad_Option : access String;
-      end record;
-
-   procedure Handle (Handler : in out Default_Handler;
-                     Value   : in     String);
-
-   function Find_Handler_By_Short_Name
-     (Short_Name : Character)
-     return Root_Argument_Handler'Class;
-
-   function Find_Handler_By_Long_Name
-     (Long_Name : String)
-     return Root_Argument_Handler'Class;
-
-   --------------
-   -- Argument --
-   --------------
-
-   function Argument (Index : Positive) return String is
-   begin
-      return Processed_Command_Line.Element (Index);
-   end Argument;
-
-   --------------------
-   -- Argument_Count --
-   --------------------
-
-   function Argument_Count return Natural is
-   begin
-      return Processed_Command_Line.Last_Index;
-   end Argument_Count;
-
-   ----------------
-   -- Exit_After --
-   ----------------
-
-   function Exit_After (Handler : Root_Argument_Handler) return Boolean is
-      pragma Unreferenced (Handler);
-   begin
-      return False;
-   end Exit_After;
-
-   --------------------------------
-   -- Find_Handler_By_Long_Name --
-   --------------------------------
-
-   function Find_Handler_By_Long_Name
-     (Long_Name : String)
-     return Root_Argument_Handler'Class
+   function Find_Option
+     (Long_Name  : String;
+      Short_Name : Character)
+     return String
    is
    begin
-      for H of Handlers loop
-         if H.Long_Name.all = Long_Name then
-            return H.Handler.all;
-         end if;
-      end loop;
-      return D : Default_Handler do
-        D.Bad_Option := new String'(Long_Name);
-      end return;
-   end Find_Handler_By_Long_Name;
-
-   --------------------------------
-   -- Find_Handler_By_Short_Name --
-   --------------------------------
-
-   function Find_Handler_By_Short_Name
-     (Short_Name : Character)
-     return Root_Argument_Handler'Class
-   is
-   begin
-      for H of Handlers loop
-         if H.Short_Name = Short_Name then
-            return H.Handler.all;
-         end if;
-      end loop;
-      return D : Default_Handler do
-         D.Bad_Option := new String'((1 => Short_Name));
-      end return;
-   end Find_Handler_By_Short_Name;
-
-   ------------
-   -- Handle --
-   ------------
-
-   procedure Handle
-     (Handler : in out Flag_Argument_Handler;
-      Value   : in     String)
-   is
-   begin
-      if Value = "" or else Value = "false" then
-         Flag_Argument_Handler'Class (Handler).Handle_Clear;
-      else
-         Flag_Argument_Handler'Class (Handler).Handle_Set;
-      end if;
-   end Handle;
-
-   ------------
-   -- Handle --
-   ------------
-
-   procedure Handle
-     (Handler : in out Dispatch_Handler;
-      Value   : in     String)
-   is
-      pragma Unreferenced (Value);
-   begin
-      Dispatch_Handler'Class (Handler).Execute;
-   end Handle;
-
-   ------------
-   -- Handle --
-   ------------
-
-   procedure Handle (Handler : in out Default_Handler;
-                     Value   : in     String)
-   is
-      pragma Unreferenced (Value);
-   begin
-      Ada.Text_IO.Put_Line
-        (Ada.Text_IO.Standard_Error,
-         "Unknown option: " & Handler.Bad_Option.all);
-      Show_Usage;
-   end Handle;
-
-   ------------------
-   -- Has_Argument --
-   ------------------
-
-   function Has_Argument (Handler : Root_Argument_Handler) return Boolean is
-      pragma Unreferenced (Handler);
-   begin
-      return False;
-   end Has_Argument;
-
-   --------------------------
-   -- Process_Command_Line --
-   --------------------------
-
-   procedure Process_Command_Line is
-      Skip_Rest      : Boolean := False;
-      Tried_Dispatch : Boolean := False;
-      Arg_Index      : Positive := 1;
-      Dispatcher     : access Dispatch_Handler'Class;
-   begin
-      while Arg_Index <= Ada.Command_Line.Argument_Count loop
+      for I in 1 .. Ada.Command_Line.Argument_Count loop
          declare
-            Item : constant String :=
-              Ada.Command_Line.Argument (Arg_Index);
+            Argument : constant String :=
+              Ada.Command_Line.Argument (I);
+            Separator_Index : constant Natural :=
+              Ada.Strings.Fixed.Index (Argument, "=");
          begin
-            if not Skip_Rest
-              and then Item (Item'First) = '-'
-            then
-               if Item'Length = 1 then
-                  Show_Usage;
-                  return;
-               elsif Item = "--" then
-                  Skip_Rest := True;
-               elsif Item (Item'First + 1) = '-' then
-                  declare
-                     Handler : Root_Argument_Handler'Class :=
-                       Find_Handler_By_Long_Name
-                       (Item (Item'First + 2 .. Item'Last));
-                  begin
-                     if Handler.Has_Argument then
-                        Arg_Index := Arg_Index + 1;
-                        Handler.Handle (Ada.Command_Line.Argument (Arg_Index));
-                     else
-                        Handler.Handle ("true");
-                     end if;
-                     exit when Handler.Exit_After;
-                  end;
-
-               elsif Item'Length = 2 then
-                  declare
-                     Handler : Root_Argument_Handler'Class :=
-                       Find_Handler_By_Short_Name (Item (Item'Last));
-                  begin
-                     if Handler.Has_Argument then
-                        Arg_Index := Arg_Index + 1;
-                        Handler.Handle (Ada.Command_Line.Argument (Arg_Index));
-                     else
-                        Handler.Handle ("true");
-                     end if;
-                     exit when Handler.Exit_After;
-                  end;
+            if Short_Name /= ' ' and then Argument = ('-', Short_Name) then
+               if I < Ada.Command_Line.Argument_Count then
+                  return Ada.Command_Line.Argument (I + 1);
                else
-                  for J in Item'First + 1 .. Item'Last loop
-                     declare
-                        Handler : Root_Argument_Handler'Class :=
-                          Find_Handler_By_Short_Name (Item (J));
-                     begin
-                        Handler.Handle ("true");
-                        exit when Handler.Exit_After;
-                     end;
-                  end loop;
+                  return "";
                end if;
-            elsif not Tried_Dispatch then
-               declare
-                  Found : Boolean := False;
-               begin
-                  for D of Dispatchers loop
-                     if D.Long_Name.all = Item then
-                        Found := True;
-                        Dispatcher :=
-                          Dispatch_Handler'Class (D.Handler.all)'Access;
-                        Processed_Command_Line.Clear;
-                        exit;
-                     end if;
-                  end loop;
-                  if not Found then
-                     Processed_Command_Line.Append (Item);
-                  end if;
-               end;
-
-               Tried_Dispatch := True;
-
-            else
-               Processed_Command_Line.Append (Item);
+            elsif Argument'Length > 3 and then Separator_Index > 0
+              and then Argument (1 .. 2) = "--"
+              and then Argument (3 .. Separator_Index - 1) = Long_Name
+            then
+               return Argument (Separator_Index + 1 .. Argument'Last);
             end if;
          end;
-
-         Arg_Index := Arg_Index + 1;
-
       end loop;
+      return "";
+   end Find_Option;
 
-      if Dispatcher /= null then
-         Dispatcher.Execute;
-      end if;
+   -----------------
+   -- Find_Option --
+   -----------------
 
-   end Process_Command_Line;
+   function Find_Option
+     (Long_Name  : String;
+      Short_Name : Character)
+     return Boolean
+   is
+   begin
+      for I in 1 .. Ada.Command_Line.Argument_Count loop
+         declare
+            Argument : constant String :=
+              Ada.Command_Line.Argument (I);
+         begin
+            if Argument = "--" & Long_Name then
+               return True;
+            elsif Argument = "--no-" & Long_Name then
+               return False;
+            elsif Argument'Length >= 2
+              and then Short_Name /= ' '
+              and then Argument (Argument'First) = '-'
+              and then Argument (Argument'First + 1) /= '-'
+            then
+               for Item of Argument loop
+                  if Item /= '-' and then Item = Short_Name then
+                     return True;
+                  end if;
+               end loop;
+            end if;
+         end;
+      end loop;
+      return False;
+   end Find_Option;
 
-   --------------
-   -- Register --
-   --------------
+   -----------------
+   -- Find_Option --
+   -----------------
 
-   procedure Register
+   function Find_Option
      (Long_Name  : String;
       Short_Name : Character;
-      Handler    : Root_Argument_Handler'Class)
+      Default    : Integer)
+     return Integer
    is
+      Value : constant String := Find_Option (Long_Name, Short_Name);
    begin
-      Handlers.Append ((new Root_Argument_Handler'Class'(Handler),
-                        new String'(Long_Name),
-                        Short_Name));
-   end Register;
-
-   --------------
-   -- Register --
-   --------------
-
-   procedure Register
-     (Long_Name  : String;
-      Handler    : Root_Argument_Handler'Class)
-   is
-   begin
-      if Handler in Dispatch_Handler'Class then
-         Dispatchers.Append ((new Root_Argument_Handler'Class'(Handler),
-                              new String'(Long_Name),
-                              Character'Val (0)));
+      if Value = "" then
+         return Default;
       else
-         Register (Long_Name, Character'Val (0), Handler);
+         return Integer'Value (Value);
       end if;
-   end Register;
-
-   --------------
-   -- Register --
-   --------------
-
-   procedure Register
-     (Short_Name : Character;
-      Handler    : Root_Argument_Handler'Class)
-   is
-   begin
-      Register ("", Short_Name, Handler);
-   end Register;
-
-   ----------------
-   -- Show_Usage --
-   ----------------
-
-   procedure Show_Usage is
-   begin
-      Ada.Text_IO.Put (Ada.Text_IO.Standard_Error,
-                       "Usage: "
-                         & Ada.Command_Line.Command_Name
-                         & " [options]");
-   end Show_Usage;
+   exception
+      when Constraint_Error =>
+         return Default;
+   end Find_Option;
 
 end WL.Command_Line;
--- src/wl-command_line.ads
+++ src/wl-command_line.ads
@@ -1,56 +1,19 @@
 package WL.Command_Line is
 
-   type Root_Argument_Handler is abstract tagged private;
+   function Find_Option
+     (Long_Name  : String;
+      Short_Name : Character)
+     return Boolean;
 
-   procedure Handle (Handler : in out Root_Argument_Handler;
-                     Value   : in     String)
-      is abstract;
+   function Find_Option
+     (Long_Name  : String;
+      Short_Name : Character)
+     return String;
 
-   function Has_Argument (Handler : Root_Argument_Handler) return Boolean;
-
-   function Exit_After (Handler : Root_Argument_Handler) return Boolean;
-
-   procedure Register (Long_Name  : String;
-                       Short_Name : Character;
-                       Handler    : Root_Argument_Handler'Class);
-
-   procedure Register (Long_Name  : String;
-                       Handler    : Root_Argument_Handler'Class);
-
-   procedure Register (Short_Name : Character;
-                       Handler    : Root_Argument_Handler'Class);
-
-   procedure Process_Command_Line;
-   procedure Show_Usage;
-
-   function Argument_Count return Natural;
-   function Argument (Index : Positive) return String;
-
-   type Flag_Argument_Handler is
-     abstract new Root_Argument_Handler with private;
-
-   procedure Handle (Handler : in out Flag_Argument_Handler;
-                     Value   : in     String);
-
-   procedure Handle_Set (Handler : in out Flag_Argument_Handler) is abstract;
-   procedure Handle_Clear (Handler : in out Flag_Argument_Handler) is null;
-
-   type Dispatch_Handler is abstract new Root_Argument_Handler with private;
-
-   procedure Execute (Handler : Dispatch_Handler)
-      is abstract;
-
-   procedure Handle (Handler : in out Dispatch_Handler;
-                     Value   : in     String);
-
-private
-
-   type Root_Argument_Handler is abstract tagged null record;
-
-   type Flag_Argument_Handler is abstract new Root_Argument_Handler
-     with null record;
-
-   type Dispatch_Handler is
-     abstract new Root_Argument_Handler with null record;
+   function Find_Option
+     (Long_Name  : String;
+      Short_Name : Character;
+      Default    : Integer)
+     return Integer;
 
 end WL.Command_Line;
