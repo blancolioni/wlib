f849fe5374b01b485d1a3da82cb37c0591ac53cd
Fraser Wilson <blancolioni@gmail.com>
2017-10-27 16:07:19 +0200

    WL.Images: added image writer interface

--- src/wl-images.adb
+++ src/wl-images.adb
@@ -71,4 +71,25 @@ package body WL.Images is
       Image.Layers (Layer).Data.Reference.Element (X, Y) := Color;
    end Set_Color;
 
+   -----------
+   -- Write --
+   -----------
+
+   procedure Write
+     (Writer : Image_Writer'Class;
+      Path   : String;
+      Image  : Image_Type'Class)
+   is
+      File : WL.Binary_IO.File_Type;
+   begin
+      WL.Binary_IO.Create (File, WL.Binary_IO.Out_File, Path);
+      Writer.Write (File, Image);
+      WL.Binary_IO.Close (File);
+   exception
+      when E : others =>
+         raise Constraint_Error with
+           "Unable to write to '"
+           & Path & "': " & Ada.Exceptions.Exception_Message (E);
+   end Write;
+
 end WL.Images;
